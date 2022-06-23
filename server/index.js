b4d2fb21a754b8973aadfb8c6716f7f062d997ff
abhisheksatpute369 <abhisheksatpute99@gmail.com>
2022-06-23 17:24:40 +0530

    user schema and model

--- server/index.js
+++ server/index.js
@@ -3,21 +3,33 @@ const express  = require("express");
 const cors = require("cors");
 
 
-// middlewares
+// middlewares start
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded())
 app.use(cors());
+//middleware end
 
-// database connection
+// database connection start
 mongoose.connect("mongodb://localhost:27017/registerstudent",{
     useNewUrlParser: true,
     useUnifiedTopology: true
 },()=>{
     console.log("database connected succefully!")
-}) 
+})
+//database connection end 
 
-//routes
+//define user schema
+const userSchema = new mongoose.Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+//routes start
 app.post("/login", (req, res) => {
     res.send("my login info");
 })
@@ -29,3 +41,4 @@ app.post("/signup", (req, res) => {
 app.listen(9002, () => {
     console.log("Listen on port 9002");
 })
+//Routes end
