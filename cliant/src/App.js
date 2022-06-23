dc45e5849793e4d14fcfe8fd3b879fb284d28f99
abhisheksatpute369 <abhisheksatpute99@gmail.com>
2022-06-23 15:20:26 +0530

    home page created

--- cliant/src/App.js
+++ cliant/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Login from './components/login';
 import Signup from './components/signup';
+import Home from "./components/home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" exact element={<Signup />} />
         <Route path="/login" exact element={<Login />} />
+        <Route path="/home" exact element={<Home />} />
 		</Routes>
     </BrowserRouter>
     </>
---
+++ cliant/src/components/home.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import "../css/home.css"
+const Home = () => {
+
+    const [exam, setexam] = useState([]);
+    const handleselect = (e) =>{
+        const selected = e.target.value;
+        if(selected === "neet")
+        {
+            setexam(["NEET Physics", "NEET Chemistry", "NEET Biology"])
+        }
+        else if(selected === "jeemain")
+        {
+            setexam(["JEE Physics", "JEE Chemistry", "JEE Biology"])
+        }
+        else if(selected === "jeeadvance")
+        {
+            setexam(["ADV Physics", "ADV Chemistry", "ADV Biology"])
+        }
+    }
+
+    const handleexam = (e) =>{
+        const examselected = e.target.value;
+        console.log(examselected)
+    }
+    return (
+        <div id="homecontainer">
+            <div className='examselect'>
+                <p>Select your paper</p>
+                <select className='selectpaper' onChange={handleselect}>
+                    <option>--Select--</option>
+                    <option value="neet">NEET</option>
+                    <option value="jeemain">JEE Main</option>
+                    <option value="jeeadvance">JEE Advance</option>
+                </select>
+                <p>Select your exam</p>
+                <select className='selectexam' onChange={handleexam}>
+                    <option >--Select--</option>
+                    {
+                        exam.map((event)=>{
+                            return(
+                                <option key={event} value={event}>{event}</option>
+                            )
+                        })
+                    }
+                </select>
+                <button className='starttest'>START TEST</button>
+            </div>
+            <h2 id="homemessege">Welcome Folk! please select your Exam and start MOCK TEST </h2>
+            <div id="line"></div>
+        </div>
+    );
+};
+
+export default Home;
